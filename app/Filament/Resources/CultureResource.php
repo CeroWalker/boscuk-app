82a9727fd3eda89f1a928e87cf8b6ff9f84ccec3
Cero <65628815+CeroWalker@users.noreply.github.com>
2024-05-26 22:40:37 +0300

    some update

--- app/Filament/Resources/CultureResource.php
+++ app/Filament/Resources/CultureResource.php
@@ -68,6 +68,8 @@ class CultureResource extends Resource
             ->columns([
                 TextColumn::make('title'),
                 TextColumn::make('title_id'),
+				TextColumn::make( 'created_at')->label('Created At'),
+                TextColumn::make( 'updated_at')->label('Edited At'),
             ])
             ->filters([
                 //
