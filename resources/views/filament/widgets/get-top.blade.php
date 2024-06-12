26354e5cf33df947ce483da97f1fc805cf2f78ec
Cero <65628815+CeroWalker@users.noreply.github.com>
2024-06-12 13:26:08 +0300

    undo

--- resources/views/filament/widgets/get-top.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament-widgets::widget>
-    <x-filament::section>
-        <@php
-            $output = shell_exec('ps -e');
-            echo "<pre style:'width:300px'>$output</pre>";
-        @endphp
-    </x-filament::section>
-</x-filament-widgets::widget>
