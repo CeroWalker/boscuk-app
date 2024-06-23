054fd027f59236ee7d7e2485e6462c9f61c6f843
Cero <65628815+CeroWalker@users.noreply.github.com>
2024-06-23 13:39:15 +0300

    Update games.blade.php

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -13,8 +13,11 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-20 md:mt-10">
 
     @foreach($games as $game)
-        <div id="guide" class="bg-white rounded-xl shadow-md overflow-hidden">
-            <img id="content_image" class="w-full object-cover" src="{{ config('app.url') }}/storage/content-image/{{ $games->content_image }}">
+        <div id="games" class="bg-white rounded-xl shadow-md overflow-hidden">
+
+            @if($games->content_image != null)
+                <img id="content_image" class="w-full object-cover" src="{{ config('app.url') }}/storage/content-image/{{ $game->content_image }}">
+            @endif
             <a class="text-gray-500 text-sm" href="{{ config('app.url') }}/oyunlar/{{ $game->title_id }}">
                 <div class="p-4">
                     <div class="text-lg font-medium text-gray-800 mb-2">{{ $game->title }}</div>
